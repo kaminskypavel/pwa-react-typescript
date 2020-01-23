71b27f9c8e011269a84e58c0e04e9b3c03e4e12f
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2020-01-23 10:23:17 +0200

    chore: add .gitignore

--- src/index.tsx
+++ src/index.tsx
@@ -10,3 +10,19 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.register();
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js');
+
+    window.addEventListener('beforeinstallprompt', (event) => {
+        event.preventDefault();
+        const button = document.querySelector('button') as HTMLButtonElement;
+        button.removeAttribute('hidden');
+        button.addEventListener('click', () => {
+            // @ts-ignore
+            event.prompt();
+            button.setAttribute('disabled', String(true));
+        });
+    });
+}
